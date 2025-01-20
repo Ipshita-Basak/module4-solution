51e71b066608367c47dcd736f7e8432be91225b3
Ipshita-Basak <idharmajumder@gmail.com>
2025-01-20 11:13:40 +0530

    app.js

--- JS/app.js
+++ JS/app.js
@@ -1,1 +1,17 @@
 
+(function () {
+   var names = ["Ipshita"];
+
+   for (var i = 0; i < names.length; i++) {
+
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+
+  if (firstLetter === 'j') {
+    byeSpeaker.speak(names[i]);    // Say "Good Bye"
+  } else {
+    helloSpeaker.speak(names[i]);  // Say "Hello"
+  }
+}
+
+})();
