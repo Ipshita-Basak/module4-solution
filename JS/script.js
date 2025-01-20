1cb5991ab6e15bdc15cefe750effefee17e53213
Ipshita-Basak <idharmajumder@gmail.com>
2025-01-20 11:49:12 +0530

    script.js

--- JS/script.js
+++ JS/script.js
@@ -10,16 +10,16 @@
 // or "j", otherwise, the program should say "Good Bye". So, the final output
 // on the console should look like this:
 /*
-Hello Ipshita
-Good Bye Ipshita
-Good Bye Ipshita
-Good Bye Ipshita
-Hello Ipshita
-Hello Ipshita
-Hello Ipshita
-Hello Ipshita
-Hello Ipshita
-Good Bye Ipshita
+Hello Yaakov
+Good Bye John
+Good Bye Jen
+Good Bye Jason
+Hello Paul
+Hello Frank
+Hello Larry
+Hello Paula
+Hello Laura
+Good Bye Jim
 
 WARNING!!! WARNING!!!
 The code does NOT currently work! It is YOUR job to make it work
@@ -35,7 +35,7 @@ WARNING!!! WARNING!!!
   // See Lecture 52, part 2
   // (Note, Step 2 will be done in the SpeakHello.js file.)
 
-  var names = ["Ipshita"];
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
   // STEP 10:
   // Loop over the names array and say either 'Hello' or "Good Bye"
