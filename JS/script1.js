791e088adbf589ad1871f6366b1aa1ff3fd0fc67
Ipshita-Basak <idharmajumder@gmail.com>
2025-01-20 11:15:01 +0530

    script1.js

--- JS/script1.js
+++ JS/script1.js
@@ -1,1 +1,12 @@
 
+(function (window) {
+
+   var helloSpeaker = {};
+   var speakWord = "Hello ";
+   helloSpeaker.speak = function(names) {
+       console.log(speakWord + " " + names);
+   };
+
+   window.helloSpeaker = helloSpeaker;
+
+ }) (window);
